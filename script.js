fb112bb72794f2c20fbb9893a725cf611f056b11
sitiawangsih <sitiawangsih276@gmail.com>
2026-09-22 13:32:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const btn=document.querySelector('.menu-btn');const nav=document.querySelector('nav');btn?.addEventListener('click',()=>{nav.style.display=nav.style.display==='flex'?'none':'flex';nav.style.flexDirection='column';nav.style.position='absolute';nav.style.top='72px';nav.style.right='4%';nav.style.background='#fff';nav.style.padding='18px';nav.style.borderRadius='12px';nav.style.boxShadow='0 10px 30px #0002'});
 function toggleACGallery() {
-    const gallery = document.getElementById("acGalleryMore");
+  const acMore = document.getElementById('acGalleryMore');
+  const allGalleryItems = document.querySelectorAll('.gallery > .gallery-item');
 
-    gallery.classList.toggle("show");
+  if (acMore.style.display === 'none' || acMore.style.display === '') {
+    acMore.style.display = 'grid';
+
+    allGalleryItems.forEach(item => {
+      if (!item.classList.contains('ac-gallery')) {
+        item.style.display = 'none';
+      }
+    });
+  } else {
+    acMore.style.display = 'none';
+
+    allGalleryItems.forEach(item => {
+      item.style.display = 'block';
+    });
+  }
 }
